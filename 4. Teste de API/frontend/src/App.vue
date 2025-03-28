c740303436514766562350df205f4016a98757da
httpsdinha <amandabalcacar@gmail.com>
2025-03-28 15:00:45 -0400

    style: header da página

---
+++ 4. Teste de API/frontend/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <header>
+    <HeaderSearch/>
+  </header>
+  <main>
+    <HelloWorld/>
+  </main>
+    
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import HeaderSearch from './components/HeaderSearch.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    HeaderSearch
+  }
+}
+</script>
+
+<style>
+  body{
+    margin: 0;
+  }
+  header {
+    background: linear-gradient(91deg, #653740 25.84%, #985260 100%);
+
+;
+    padding: 10px;
+    box-shadow: black 0px 0.2px 20px;
+  }
+
+</style>
---
+++ 4. Teste de API/frontend/src/components/HeaderSearch.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="header-container">
+        <img src="../assets/logo.png" class="logo" />
+        <div class="search-container">
+            <img src="../assets/search.png" class="search-icon" />
+            <input type="text" placeholder="Buscar operadora..." class="search"/>
+        </div>
+    </div>
+</template>
+
+<style>
+    .header-container {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 20px; 
+    }
+
+    .logo {
+        width: 70px;
+        height: 70px;
+        margin-left: 0; 
+        padding: 10px;
+        margin-right: 1.5em;
+    }
+
+    .search {
+        border-radius: 3px;
+        background: #FFF;
+        width: 500px;
+        height: 25px;  
+        border: none;
+        padding-left: 35px; 
+    }
+
+    .search-container {
+        display: flex;
+        position: relative;
+        align-items: center;
+        
+    }
+
+    .search-icon {
+        position: absolute;
+        left: 10px;
+        top: 50%;
+        transform: translateY(-50%);
+        width: 15px;
+        height: 15px;
+        cursor: pointer;
+    }
+</style>
